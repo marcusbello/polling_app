86f41f37350971356771dde3b8860bbadc5c8e6b
Marcus Bello <marcusthetechguru@gmail.com>
2023-06-05 07:43:17 -0400

    Changed to modelviewsets

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -2,9 +2,15 @@ from rest_framework import generics, status
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
 
 from .models import Poll, Choice
 from .serializers import PollSerializer, ChoiceSerializer, VoteSerializer
+
+
+class PollViewSet(viewsets.ModelViewSet):
+    queryset = Poll.objects.all()
+    serializer_class = PollSerializer
 
 
 class PollList(generics.ListCreateAPIView):
