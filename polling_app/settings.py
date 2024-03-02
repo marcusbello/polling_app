b4b8092137f07e78e63cad817f2916cdfdb4f3d4
Marcus Bello <marcusthetechguru@gmail.com>
2024-03-01 18:44:20 -0600

    staticfiles vercel yt

--- polling_app/settings.py
+++ polling_app/settings.py
@@ -84,7 +84,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'polling_app.wsgi.app'
+WSGI_APPLICATION = 'polling_app.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
